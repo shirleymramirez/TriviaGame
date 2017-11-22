27e510da03a4293f96ba568d8b6a744c5487d424
jheilhet <shirley.mramirez@yahoo.com>
2017-11-21 18:29:10 -0700

    modified style.css index.html and reset.css

---
+++ assets/javascript/app.js
@@ -0,0 +1,139 @@
+$(document).ready(function() {
+
+	//List of Arrays in an Objects for our questions
+	var myQuestions = [
+		{
+			question: "The llama is a domesticated camelid that is native to which continent?",
+			choices: {
+				a:"South America",
+				b:"Asia",
+				c:"Europe",
+				d:"South Africa"
+			},
+			correctAnswer: {	
+				answer: "a - South America" ,
+				imageUrl: "assets/images/yoda.jpg"
+			}
+		},
+		{
+			question: "In the children's books about a 25 foot tall red dog, what is the name of the dog?",
+			choices: {
+				a:"Goofy",
+				b:"Clifford",
+				c:"Charlie",
+				d:"Snoopy"
+			},
+			correctAnswer: {	
+				answer: "b - Clifford",
+				imageUrl: "assets/images/yoda.jpg"
+			}
+		},
+		{ 
+			question: "What does the acronym \"lol\" stand for when used in phone texts and on the internet?",
+			choices: {
+				a:"Laugh out loud",
+				b:"League of Legends",
+				c:"Lots of Love",
+				d:"Laugh or Love"
+			},
+			correctAnswer: {	
+				answer: "a - Laugh out loud",
+				imageUrl: "assets/images/yoda.jpg"
+			}
+		},
+		{
+			question: "Which musician is often called the fifth Beatle?",
+			choices: {
+				a:"John Lennon",
+				b:"Ringo Starr",
+				c:"George Harrison",
+				d:"Pete Best"
+			},
+			correctAnswer: {	
+				answer:"d - Pete Best",
+				imageUrl: "assets/images/yoda.jpg"
+			}
+		},
+		{ 
+			question: "What is professional wrestler John Cena's famous catchphrase?",
+			choices: {
+				a:"Hell yah!",
+				b:"You can't see me!",
+				c:"I'm coming",
+				d:"Catch yah!"
+			},
+			correctAnswer: {
+				answer:"b - You can't see me!",
+				imageUrl: "assets/images/yoda.jpg"
+			}
+		},
+		{
+			question: "Which President is on the United States 1,000 dollar bill?",
+			choices: {
+				a:"George Washington",
+				b:"Grover Cleveland",
+				c:"Barack Obama",
+				d:"Bill Clinton"
+			},
+			correctAnswer: {	
+				answer:"b - Grover Cleveland",
+				imageUrl: "assets/images/yoda.jpg"
+			}
+		},
+		{
+			question: "What 2013 science fiction blockbuster starred Sandra Bullock and George Clooney?",
+			choices: {
+				a:"Up in the air",
+				b:"The Proposal",
+				c:"Two Weeks Notice",
+				d:"Gravity"
+			},
+			correctAnswer: {	
+				answer:"d - Gravity",
+				imageUrl: "assets/images/yoda.jpg"
+			}
+		},
+		{
+			question: "The companies HP, Microsoft and Apple were all started in a what?",
+			choices: {
+				a:"Garage",
+				b:"Office",
+				c:"School",
+				d:"Park"
+			},
+			correctAnswer: {	
+				answer:"a - Garage",
+				imageUrl: "assets/images/yoda.jpg"
+			}
+		},
+		{
+			question: "Kopi luwak is a very expensive type of what?",
+			choices: {
+				a:"Candy",
+				b:"Milk",
+				c:"Coffee",
+				d:"Chocolates"
+			},
+			correctAnswer: {	
+				answer:"c - Coffee",
+				imageUrl: "assets/images/yoda.jpg"
+			}
+		},
+		{
+			question: "What is the largest organ of the human body?",
+			choices: {
+				a:"Liver",
+				b:"Lungs",
+				c:"Skin",
+				d:"Heart"
+			},
+			correctAnswer:{	
+				answer:"c - Skin", 
+				imageUrl: "assets/images/yoda.jpg"
+			}
+		}
+	];
+
+	// Variable Declaration
+
+});
